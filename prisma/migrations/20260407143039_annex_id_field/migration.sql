44959eb471803278122a196660788fae0feb9beb
Ilyes Mekhalfa <ilyes.mekalfa@gmail.com>
2026-04-18 03:37:05 +0100

    feat(coach): add caoch basics to the api

--- prisma/migrations/20260407143039_annex_id_field/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[annexId]` on the table `Annex` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Annex_annexId_key" ON "Annex"("annexId");
---
+++ src/coach/coach.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { CoachController } from './coach.controller';
+import { CoachService } from './coach.service';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Module({
+  controllers: [CoachController],
+  providers: [CoachService, PrismaService]
+})
+export class CoachModule {}
