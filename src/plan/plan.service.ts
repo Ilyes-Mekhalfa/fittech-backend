fb666f40162665a97e5452e0e5b9402fa2e8e118
Ilyes Mekhalfa <ilyes.mekalfa@gmail.com>
2026-04-27 10:11:48 +0100

    feat(plan): add plan core service closes #27

---
+++ src/plan/plan.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class PlanService {
+
+    constructor( private prismaService: PrismaService){}
+
+    async getAllPlans(){
+        return await this.prismaService.fitapi_subscriptionplan.findMany()
+    }
+
+    async getPlan(id: string){
+        return await this.prismaService.fitapi_subscriptionplan.findUnique({
+            where: { id }
+        })
+    }
+
+    async addPlan(body: any){
+        return await this.prismaService.fitapi_subscriptionplan.create({
+            data: body
+        })
+    }
+
+    async updatePlan(id: string, body: any){
+        return await this.prismaService.fitapi_subscriptionplan.update({
+            where: { id },
+            data: body
+        })
+    }
+
+    async deletePlan(id: string){
+        return await this.prismaService.fitapi_subscriptionplan.delete({
+            where: { id }
+        })
+    }
+}
